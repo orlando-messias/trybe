45a08f5cce5b4c66e7b214f0921d7c06734890b1
orlandomessias <programadorlando@gmail.com>
2020-03-26 18:19:49 -0300

    exerc04 positive, negative or zero

---
+++ exercises/4_1/exerc04.js
@@ -0,0 +1,12 @@
+// Shows if the number is positive, negative or zero
+
+let verifyNumber = 0;
+let result = '';
+if(verifyNumber > 0)
+    result = 'Positive';
+else if(verifyNumber < 0)
+    result = 'Negative';
+else
+    result = 'Zero'
+
+console.log('The number is ' + result)
