7081d17f12bf71eb7d91ebf7b4e196c4cca897d6
orlandomessias <programadorlando@gmail.com>
2020-03-27 15:33:26 -0300

    exerc 01 print a square of asteriks

---
+++ exercises/4_2/exerc01.js
@@ -0,0 +1,12 @@
+// print a square of asterisks
+
+let n = 6;
+let asterisks = '';
+
+for(let i=1; i<=n; i++){
+    asterisks = '';
+    for(let j = 1; j<=n; j++){
+        asterisks += '*';
+    }
+    
+}
