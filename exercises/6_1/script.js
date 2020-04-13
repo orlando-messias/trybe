6f7c9f199050bf88f1e6b92a5b754c3e5c92f8ba
orlandomessias <programadorlando@gmail.com>
2020-04-13 16:28:02 -0300

    exerc 2 validate date field on submit form

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,6 +1,32 @@
 const state = document.getElementById('state');
 const arrStates = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"]
+const btnSubmit = document.getElementById('btn-submit');
+const startDate = document.getElementById('start-date');
 
+function onSubmit(){
+  if(startDate.value.indexOf('/') === 2 && startDate.value.indexOf('/', 3) === 5) {
+    let day = startDate.value.substr(0,2);
+    let month = startDate.value.substr(3,2);
+    let year = startDate.value.substr(6,4);
+    if ((day > 0 && day <= 31) && (month >0 && month <= 12) && (year.length === 4 && year >= 0)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function validate() {
+  event.preventDefault();
+  if (!onSubmit()) {
+    startDate.style.border = '2px solid red';
+    document.getElementById('note').innerHTML = 'Data Inválida'  
+  }
+  else {
+    startDate.style.border = '1px solid rgb(167,167,167)';
+    document.getElementById('note').innerHTML = ''  
+  }
+}
 
 for (let i = 0; i < arrStates.length; i += 1){
   let option = document.createElement('option');
@@ -9,3 +35,4 @@ for (let i = 0; i < arrStates.length; i += 1){
   option.appendChild(item);
   state.appendChild(option);
 }
+
