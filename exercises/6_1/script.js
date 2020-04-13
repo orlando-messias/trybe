b3059e8f6ecb766e24e3a275250d7da9751a53d8
orlandomessias <programadorlando@gmail.com>
2020-04-13 17:31:03 -0300

    function to validate email field

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -2,8 +2,9 @@ const state = document.getElementById('state');
 const arrStates = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"]
 const btnSubmit = document.getElementById('btn-submit');
 const startDate = document.getElementById('start-date');
+let email = document.getElementById('email');
 
-function onSubmit(){
+function dateValidation(){
   if(startDate.value.indexOf('/') === 2 && startDate.value.indexOf('/', 3) === 5) {
     let day = startDate.value.substr(0,2);
     let month = startDate.value.substr(3,2);
@@ -16,15 +17,29 @@ function onSubmit(){
   return false;
 }
 
-function validate() {
+function emailValidation() {
+  let pattern = /[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+  return pattern.test(email.value);
+}
+
+function onSubmit() {
   event.preventDefault();
-  if (!onSubmit()) {
-    startDate.style.border = '2px solid red';
+  if (!dateValidation()) {
+    startDate.classList.add('hasError')
     document.getElementById('note').innerHTML = 'Data Inválida'  
   }
   else {
-    startDate.style.border = '1px solid rgb(167,167,167)';
+    startDate.classList.remove('hasError');
     document.getElementById('note').innerHTML = ''  
+  }
+
+  if (!emailValidation()) {
+    email.classList.add('hasError');
+    document.getElementById('email-note').style.visibility = 'visible';
+  }
+  else {
+    email.classList.remove('hasError');
+    document.getElementById('email-note').style.visibility = 'hidden';
   }
 }
 
