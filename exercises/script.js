b0153e6146f1190b13707aeb76e8ce6c3e4e3e75
orlando-messias <programadorlando@gmail.com>
2020-04-27 22:28:37 -0300

    exercise 05

--- exercises/script.js
+++ exercises/script.js
@@ -1,4 +1,5 @@
 const divExercise4 = document.getElementById('exercise-4');
+const section5 = document.getElementById('section-5');
 
 divExercise4.addEventListener('mouseover', () => {
     divExercise4.classList.add('animation');
@@ -6,4 +7,12 @@ divExercise4.addEventListener('mouseover', () => {
 
 divExercise4.addEventListener('mouseout', () => {
     divExercise4.classList.remove('animation');
+});
+
+section5.addEventListener('mouseover', () => {
+    section5.children[0].classList.add('animation');
+});
+
+section5.addEventListener('mouseout', () => {
+    section5.children[0].classList.remove('animation');
 });
