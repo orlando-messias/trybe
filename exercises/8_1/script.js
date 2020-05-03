48aab7487b2b6aed7d0b42aea528705b4784daa3
orlando-messias <programadorlando@gmail.com>
2020-05-02 22:17:46 -0300

    exerc 02

--- exercises/8_1/script.js
+++ exercises/8_1/script.js
@@ -11,3 +11,17 @@ const fatorial = (n) => {
 
 // recursivity
 const fat = n => (n === 0) ? 1 : n * fat(n-1);
+
+
+// function return the longest word
+const longestWord = (word => {
+    word = word.replace(/\.|\,/g, '')  // remove . and , characters
+    console.log(word)
+    let arrayWords = word.split(' ');
+    let longest_word = '';
+    arrayWords.forEach(w => {
+        if (w.length > longest_word.length)
+            longest_word = w;
+    });
+    return longest_word;
+});
