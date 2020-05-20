97503eb53a8768cc8e41a279dd95535824fac39d
orlando-messias <programadorlando@gmail.com>
2020-05-20 18:56:29 -0300

    index + apiScript to return jokes

---
+++ exercises/10_3/apiScript.js
@@ -0,0 +1,17 @@
+const jokeContainer = document.getElementById('jokeContainer');
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = (data) => {
+    jokeContainer.innerHTML = `${data.joke}`;
+};
+
+const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+};
+
+fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => fetchJoke(data))
+
+window.onload = () => fetchJoke();
