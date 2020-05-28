19167af631d568060474cc20284443ee6d91a396
orlando-messias <programadorlando@gmail.com>
2020-05-28 19:00:25 -0300

    call function task

---
+++ exercises/myproject/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Tecnologies to build Web Applications</p>
+          {task("React")}
+      </header>
+    </div>
+  );
+}
+
+export default App;
